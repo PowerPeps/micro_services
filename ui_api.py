323d0183ab6620587aa6a02df33260b4192c64c4
Emile Tôlerie <77052948+PowerPeps@users.noreply.github.com>
2025-02-27 10:15:51 +0100

    UI web && Cross origin request
    Ajout d'une interface web, et du cross origin pour les requetes a l'api.

--- ui_api.py
+++ ui_api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app) #autorise requetes cross origin
 
 MICROSERVICE_STOCKS = "http://127.0.0.1:5001"
 MICROSERVICE_EMPRUNTS = "http://127.0.0.1:5002"
